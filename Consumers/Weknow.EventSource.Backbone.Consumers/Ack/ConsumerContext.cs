4b8ae6364f576629ba6a3da3e08615c60b5b9929
bnaya eshet <bnayanet@hotmail.com>
2020-07-27 23:42:25 +0300

    [ADD] Ack & Segmentation implementation

---
+++ Consumers/Weknow.EventSource.Backbone.Consumers/Ack/ConsumerContext.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Threading.Tasks.Dataflow;
+
+using static System.Threading.Tasks.ValueTaskStatic;
+
+namespace Weknow.EventSource.Backbone
+{
+    /// <summary>
+    /// Base class for acknowledge-trigger.
+    /// </summary>
+    public abstract class ConsumerContext : IAck
+    {
+        private readonly CancellationTokenSource _ackByTimeout = new CancellationTokenSource();
+        private int _onlyonce = 0;
+
+        public ConsumerContext(Metadata metadata)
+        {
+            _ackByTimeout.Token.Register(() => (this as IAck).AckAsync());
+            Metadata = metadata;
+        }
+
+        public Metadata Metadata { get; }
+
+        public void AckAfter(TimeSpan timeout) => _ackByTimeout.CancelAfter(timeout);
+
+        public abstract  ValueTask OnAckAsync();
+
+        ValueTask IAck.AckAsync()
+        {
+            int once = Interlocked.Increment(ref _onlyonce);
+            if (once != 1)
+                return CompletedValueTask;
+            return OnAckAsync();
+        }
+    }
+}
---
+++ Consumers/Weknow.EventSource.Backbone.Consumers/ConsumerDefaultSegmentationStrategy.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Immutable;
+using System.Threading.Tasks;
+
+namespace Weknow.EventSource.Backbone
+{
+    public class ConsumerDefaultSegmentationStrategy :
+                        IConsumerAsyncSegmentationStrategy
+    {
+        private const string PREFIX = "@DEFAULT";
+
+        ValueTask<(bool, T)> IConsumerAsyncSegmentationStrategy.
+                                TryUnclassifyAsync<T>(
+                                        ImmutableDictionary<string, ReadOnlyMemory<byte>> segments,
+                                        string operation,
+                                        string argumentName,
+                                        IEventSourceOptions options)
+        {
+            string key = $"{PREFIX}~{operation}~{argumentName}";
+            if (segments.TryGetValue(key, out ReadOnlyMemory<byte> data))
+            {
+                T item = options.Serializer.Deserialize<T>(data);
+                return (true, item).ToValueTask();
+            }
+            return (false, default(T)).ToValueTask();
+        }
+    }
+}
