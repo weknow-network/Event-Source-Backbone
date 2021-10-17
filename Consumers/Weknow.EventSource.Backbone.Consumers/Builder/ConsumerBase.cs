2d0a5ab5b78385d6e3b1bdc453e3430af091afc6
bnaya eshet <bnayanet@hotmail.com>
2021-10-17 09:41:58 +0300

    [RFC] Cleanup

--- Consumers/Weknow.EventSource.Backbone.Consumers/Builder/ConsumerBase.cs
+++ Consumers/Weknow.EventSource.Backbone.Consumers/Builder/ConsumerBase.cs
@@ -58,44 +58,4 @@ namespace Weknow.EventSource.Backbone
 
         #endregion // Subscribe
     }
-    /// <summary>
-    /// Base class for the consumer's code generator
-    /// </summary>
-    /// <typeparam name="T"></typeparam>
-    public partial class ConsumerBase<T>
-    {
-        private readonly IConsumerPlan _plan;
-        private readonly T _handler;
-
-        #region Ctor
-
-        /// <summary>
-        /// Initializes a new instance.
-        /// </summary>
-        /// <param name="plan">The plan.</param>
-        /// <param name="handler">The events handler.</param>
-        public ConsumerBase(
-            IConsumerPlanBuilder plan,
-            T handler)
-        {
-            _plan = plan.Build();
-            _handler = handler;
-        }
-
-        #endregion // Ctor
-
-        #region Subscribe
-
-        /// <summary>
-        /// Subscribes this instance.
-        /// </summary>
-        /// <returns></returns>
-        public IConsumerLifetime Subscribe()
-        {
-            var subscription = new Subscription(_plan, _handler);
-            return subscription;
-        }
-
-        #endregion // Subscribe
-    }
 }
