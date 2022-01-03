49ba952d6c8e8b61957b6fe4a5c97b98189c0027
Bnaya Eshet <3382669+bnayae@users.noreply.github.com>
2022-01-03 15:39:10 +0200

    [UPD] Delay when empty default

--- Channels/REDIS/Weknow.EventSource.Backbone.Channels.RedisConsumerProvider/Setting/DelayWhenEmptyBehavior.cs
+++ Channels/REDIS/Weknow.EventSource.Backbone.Channels.RedisConsumerProvider/Setting/DelayWhenEmptyBehavior.cs
@@ -29,15 +29,20 @@ namespace Weknow.EventSource.Backbone.Channels.RedisProvider
         /// <summary>
         /// Gets or sets the maximum delay.
         /// </summary>
-        public TimeSpan MaxDelay { get; init; } = TimeSpan.FromSeconds(5);
+        public TimeSpan MaxDelay { get; set; } = TimeSpan.FromSeconds(5);
 
 
         /// <summary>
         /// Gets or sets the next delay.
         /// </summary>
-        public Func<TimeSpan, TimeSpan> CalcNextDelay { get; init; } = DefaultCalcNextDeay;
+        public Func<TimeSpan, TimeSpan> CalcNextDelay { get; set; } = DefaultCalcNextDelay;
 
-        private static TimeSpan DefaultCalcNextDeay(TimeSpan previous)
+        /// <summary>
+        /// Default calculation of next delay.
+        /// </summary>
+        /// <param name="previous">The previous delay.</param>
+        /// <returns></returns>
+        private static TimeSpan DefaultCalcNextDelay(TimeSpan previous)
         {
             var prevMilli = previous.TotalMilliseconds;
             var milli = Max(prevMilli * 2, 10);
